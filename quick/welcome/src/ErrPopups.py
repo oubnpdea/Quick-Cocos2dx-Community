a5b19bcad69f16ada5dcea7edc69b31108f5b5bf
oubnpdea <uibneadtnk@gmail.com>
2016-03-05 16:23:05 +0800

    Update ErrPopups.py

--- quick/welcome/src/ErrPopups.py
+++ quick/welcome/src/ErrPopups.py
@@ -2,6 +2,9 @@ from Tkinter import *
 root = Tk()
 root.title('Quick Player Error Popups')
 root.geometry('400x100')   
-
-w = Label(root, text="Please check whether the 'X:\Users\XXX\Documents\.quick_player.lua'(windows) file contains unrecognized characters, or other errors can't read!", width = 400,height = 100,wraplength = 400,anchor = 'center').pack()
+wt = ''
+import sys
+for i in range(1, len(sys.argv)):
+	wt = wt + " " + sys.argv[i]
+w = Label(root, text="Please check whether the '"+wt+"' file contains unrecognized characters, or other errors can't read!", width = 400,height = 100,wraplength = 400,anchor = 'center').pack()
 root.mainloop()
